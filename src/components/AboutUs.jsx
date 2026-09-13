7e56f1acfa1d2339a0f4a63b3ee7f3bd60d6d1fd
saritask <kalshettysarita9@gmail.com>
2026-09-13 19:01:28 +0530

    Create AboutUs.jsx

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,24 @@
+function AboutUs() {
+  return (
+    <div className="about-us">
+      <h1>About Paradise Nursery</h1>
+
+      <p>
+        Paradise Nursery is an online plant shop dedicated to bringing
+        beautiful and healthy plants into homes and offices.
+      </p>
+
+      <p>
+        We offer a wide variety of indoor plants, succulents, and
+        flowering plants for plant lovers of all experience levels.
+      </p>
+
+      <p>
+        Our goal is to make it easy for everyone to discover and enjoy
+        the beauty of nature.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
